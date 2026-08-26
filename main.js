25526cdca1d2674ec88ee07939be6cbad65da820
Peerless Geek <thepeerlessgeek@gmail.com>
2026-08-26 18:44:00 +0530

    Update August 2026 coupon codes (3 of 3)

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
    1. LINK CONFIGURATION & INJECTION
    ========================================= */
 const LINKS = { 
-  java: "https://www.udemy.com/course/the-ultimate-animated-java-interview-questions-course/?couponCode=JAVAAUG2602", 
-  blind75: "https://www.udemy.com/course/blind-75-leetcode-the-animated-aura-series-crack-dsa/?couponCode=BLIND75AUG2602", 
+  java: "https://www.udemy.com/course/the-ultimate-animated-java-interview-questions-course/?couponCode=JAVAAUG2603", 
+  blind75: "https://www.udemy.com/course/blind-75-leetcode-the-animated-aura-series-crack-dsa/?couponCode=BLIND75AUG2603", 
   instagram: "https://www.instagram.com/peerlessgeek/", 
   youtube: "https://www.youtube.com/@peerlessgeek", 
   facebook: "https://facebook.com",
